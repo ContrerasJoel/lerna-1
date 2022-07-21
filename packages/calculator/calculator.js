547454c3a7ffec4944bed86f2a1294ea13953f1b
Joel Contreras <jocontreras246@gmail.com>
2022-07-21 04:00:26 -0400

    segundo

---
+++ packages/calculator/calculator.js
@@ -0,0 +1,11 @@
+const basicsOperations = require('@testing-lerna/basics-operations');
+const advancedOperations = require('@testing-lerna/advanced-operations');
+const showValue = require('@testing-lerna/common');
+
+function factorial2x(value) {
+  const double = basicsOperations.multiply(value, 2);
+  return advancedOperations.factorial(double);
+}
+
+showValue(factorial2x(10));
+showValue(advancedOperations.summation(10, 30));
