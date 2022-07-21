873b673ad04f7cf009f3b1cb8ec74dfb04708b32
Joel Contreras <jocontreras246@gmail.com>
2022-07-21 09:20:56 -0400

    Button

---
+++ packages/component/node_modules/root/packages/advanced-operations/advanced.js
@@ -0,0 +1,21 @@
+
+
+const basicOperations = require('@testing-lerna/basics-operations');
+
+function factorial(a) {  
+  if (a === 1) return 1;
+  return basicOperations.multiply (a, factorial(a -1));
+}
+
+function summation(value, times) {
+  let result = value;
+
+  for (let i = 0; i < times; ++i) {
+    result = basicOperations.add(result, value);
+  }
+  
+  return result;
+}
+
+exports.factorial = factorial;
+exports.summation = summation;
